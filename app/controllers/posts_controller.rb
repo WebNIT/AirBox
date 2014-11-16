ef5127d41c65d4240a315f51ecca4b466df68ca3
Aruz Parajuli <arujparajuli@gmail.com>
2014-11-17 01:58:24 +0530

    Building post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_filter :authenticate_user!, except: [:show, :index]
 
     def index
         @posts = Post.all
@@ -8,4 +9,25 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
-end
+    def new
+        @post = Post.new
+    end
+
+    def create
+
+    	@user = current_user
+    	@post = @user.posts.build(allowed_params)
+
+    	if @post.save
+      		flash[:success] = "Created new post"
+      		redirect_to @post
+    	else
+      		render 'new'
+    	end
+    end
+
+    private
+        def allowed_params
+            params.require(:post).permit(:title, :body)
+        end
+end 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 30 }
 end
