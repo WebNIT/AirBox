75e308ccc0514e22a623bd94c0b020b7c9ec4a6d
Aruz Parajuli <arujparajuli@gmail.com>
2014-11-17 02:19:26 +0530

    Final touch #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(30)
+    @sent_invites = current_user.sent_invites.page(params[:page])
+    @received_invites = current_user.received_invites.page(params[:page])
   end
 
   def show
